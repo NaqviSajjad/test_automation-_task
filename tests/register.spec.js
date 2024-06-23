05ad08d31878770f5a101416b8d8985bcd90f34f
NaqviSajjad <samn0077@gmail.com>
2024-06-23 21:07:12 +0200

    Update register.spec.js

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -1,17 +1,33 @@
 const { test, expect } = require('@playwright/test');
+const { RegisterPage } = require('../e2e/pages/registerPage');
+const { faker } = require('@faker-js/faker');
 
-import { RegisterPage } from '../e2e/pages/register';
-import * as data from '../testData/credentials.json'
+test.describe('Registration Tests', () => {
+  test('Create Account', async ({ page }) => {
+    const registerPage = new RegisterPage(page);
 
-test('Create Account', async ({ page }) => {
-    const Register = new RegisterPage(page)
-    await Register.gotoRegisterPage()
-    await expect(Register.assertion1).toBeVisible()
+    // Generate random user data using Faker
+    const userData = {
+      gender: 'Mr.',
+      firstName: faker.person.firstName('male'),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      day: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).getDate().toString(),
+      month: (faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).getMonth() + 1).toString(), // getMonth() returns 0-11
+      year: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).getFullYear().toString()
+    };
 
-    await Register.registrationProcess(data[1].username)
-    console.log('Register credentials:', data[1].username);
+    await registerPage.gotoRegisterPage();
+    await expect(registerPage.selectors.assertionAuthentication).toBeVisible();
 
-  
+    await registerPage.registrationProcess(userData.email, userData);
+    await page.waitForTimeout(1000)
+    const currentUrl = page.url();
+    console.log(currentUrl)
 
+    await expect(currentUrl).toEqual("http://www.automationpractice.pl/index.php?controller=my-account")
 
+    console.log('Registering with credentials:', currentUrl);
+  });
 });
